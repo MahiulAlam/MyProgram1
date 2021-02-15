2dc4af068caf615db768d9c66479df83efa144f4
Mahiul Alam Sad <77919382+MahiulAlam@users.noreply.github.com>
2021-02-15 22:59:28 +0600

    Add files via upload

---
+++ FDFD/fdfd.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<form action="results.php" method="post">
+Enter your first name: <input type="text" name="fname">
+Enter your Email: <input type="text"name="email">
+<input type="submit" value="SUBMIT">
+</form>
+</body>
+</html>
